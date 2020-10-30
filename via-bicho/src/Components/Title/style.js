3df455316b293f73fead885a7e04ab214c36b89f
Arthur P Baumgardt <57924116+4rthb@users.noreply.github.com>
2020-10-30 16:29:11 -0300

    Revert "Adoption fair section"

--- via-bicho/src/Components/Title/style.js
+++
@@ -1,8 +0,0 @@
-import styled from 'styled-components'
-
-export const Image = styled.img`
-    width: 390px;
-    height: 220px;
-    border-radius: 50px 50px 0 0;
-    filter: drop-shadow(5px 5px 15px rgba(0, 0, 0, 0.25));
-`
